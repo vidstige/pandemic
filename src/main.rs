f59129cecd5871e0ee882287e1d261f38e4673ef
Samuel Carlsson <samuel.carlsson@volumental.com>
2021-09-02 10:26:37 +0200

    draw cards

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-const CITIES: usize = 48;
+use std::fmt;
+
 const DISEASES: usize = 4;
 
 #[derive(Clone)]
@@ -6,16 +7,84 @@ enum Card {
     City(usize),
 }
 
+const CITIES: &[&str] = &[
+  "Atlanta",
+  "San Franscisco",
+  "Chicago",
+  "Montreál",
+  "New York",
+  "Washington",
+  "Los Angeles",
+  "Mexico City",
+  "Miami",
+  "Bogotá",
+  "Lima",
+  "São Paulo",
+  "Santiago",
+  "Buenos Aires",
+  "London",
+  "Essen",
+  "St. Petersburg",
+  "Madrid",
+  "Paris",
+  "Milan",
+  "Algiers",
+  "Istanbul",
+  "Moscow",
+  "Cairo",
+  "Baghdad",
+  "Tehran",
+  "Riyadh",
+  "Karachi",
+  "Delhi",
+  "Mumbai",
+  "Kolkata",
+  "Chennai",
+  "Lagos",
+  "Khartoum",
+  "Kinshasa",
+  "Johannesburg",
+  "Bejing",
+  "Seoul",
+  "Shanghai",
+  "Tokyo",
+  "Bankok",
+  "Hong Kong",
+  "Taipei",
+  "Osaka",
+  "Jakarta",
+  "Ho Chi Min City",
+  "Manila",
+  "Sidney",
+];
+
+impl fmt::Debug for Card {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Card::City(i) => write!(f, "{}", CITIES[*i]),
+        }
+    }
+}
+
 #[derive(Clone)]
 struct Stack {
     cards: Vec<Card>,
+}
+trait Drawable {
+    fn draw(&mut self) -> Option<Card>;
+}
+
+impl Drawable for Stack {
+    fn draw(&mut self) -> Option<Card> {
+        return self.cards.drain((self.cards.len() - 1)..).next();
+    }
 }
 
 fn empty() -> Stack {
     return Stack { cards: vec![] };
 }
 fn full() -> Stack {
-    let cards: Vec<Card> = (0..CITIES).into_iter().map(|i| Card::City(i)).collect();
+    let cards: Vec<Card> = (0..CITIES.len()).into_iter().map(|i| Card::City(i)).collect();
     return Stack { cards: cards };
 }
 
@@ -27,14 +96,16 @@ struct Disease {
 struct State {
     hands: Vec<Stack>,
     player_cards: Stack,
+    player_discard: Stack,
     infection_cards: Stack,
     infection_discard: Stack,
     diseases: [Disease; DISEASES],
-    cubes: [u32; CITIES],
+    cubes: [u32; CITIES.len()],
     outbreaks: i32,
 }
 
-fn initial(players: usize) -> State  {
+// Create an empty game state with unshuffled decks, etc.
+fn create(players: usize) -> State  {
     let diseases = [
         Disease { cured: false, eradicated: false},
         Disease { cured: false, eradicated: false},
@@ -44,15 +115,21 @@ fn initial(players: usize) -> State  {
     return State {
         hands: vec![empty(); players],
         player_cards: full(),
+        player_discard: empty(),
         infection_cards: full(),
         infection_discard: empty(),
         diseases: diseases,
-        cubes: [0; CITIES],
+        cubes: [0; CITIES.len()],
         outbreaks: 0,
      };
 }
 
+fn setup(state: &mut State) {
+    let card = state.infection_cards.draw();
+    println!("{:?}", card);
+}
+
 fn main() {
-    let state = initial(3);
-    println!("Hello, world!");
+    let mut state = create(3);
+    setup(&mut state);
 }
