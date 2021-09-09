005bf76174908db11bda55b105510e9618e65843
Samuel Carlsson <samuel.carlsson@volumental.com>
2021-09-09 08:51:05 +0200

    print plys

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 use rand::Rng;
 use rand::seq::SliceRandom;
+use std::fmt;
+
 mod pandemic;
 
 use pandemic::State;
+use pandemic::Ply;
 use pandemic::is_win;
 use pandemic::valid_plys;
 use pandemic::perform;
+
+
+impl fmt::Display for Ply {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Ply::Drive(city) => write!(f, "Drive to {:?}", city),
+            Ply::DirectFlight(city) => write!(f, "Direct flight to {:?}", city),
+            Ply::CharteredFlight(city) => write!(f, "Chartered flight to {:?}", city),
+            Ply::Treat(city) => write!(f, "Treat in {:?}", city),
+            Ply::Cure(disease) => write!(f, "Cure {:?}", disease),
+            Ply::Construct(city) => write!(f, "Construct station in {:?}", city),
+       }
+    }
+}
 
 fn playout(from: &State, rng: &mut impl Rng) -> i32 {
     let mut state = from.clone();
@@ -13,6 +30,7 @@ fn playout(from: &State, rng: &mut impl Rng) -> i32 {
     while is_win(&state).is_none() {
         let plys = valid_plys(&state);
         let ply = plys.choose(rng).unwrap();
+        println!("{}", ply);
         perform(&mut state, ply);
     }
     println!("outcome: {:?}", if is_win(&state).unwrap() { "win" } else { "loss" });
